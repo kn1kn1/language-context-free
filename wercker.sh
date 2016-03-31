b6d6dc7acdfba846b4968a991f5def9564309f6e
kn1kn1 <kn1kn1@users.noreply.github.com>
2016-03-31 16:48:47 +0900

    :green_heart: Change ci commands for atom-beta

--- wercker.sh
+++ wercker.sh
@@ -3,18 +3,22 @@
 apt-get update && \
 DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
 
-wget -nv -O atom-amd64.deb https://atom.io/download/deb && \
+# Download And Install Atom from Beta channel
+wget -nv -O atom-amd64.deb https://atom.io/download/deb?channel=beta && \
 DEBIAN_FRONTEND=noninteractive dpkg -i atom-amd64.deb && \
 DEBIAN_FRONTEND=noninteractive apt-get install -f && \
 rm -rf atom-amd64.deb
 
-apm --version
+# Show version with apm-beta
+apm-beta --version
 
-apm install
+# Install with apm-beta
+apm-beta install
 
+# Test with apm-beta
 start-stop-daemon --start --pidfile /tmp/xvfb_99.pid --make-pidfile \
   --background --exec /usr/bin/Xvfb -- :99 -screen 0 1024x768x24 -ac \
   +extension GLX +extension RANDR +render -noreset && \
 sleep 3 && \
 export DISPLAY=:99 && \
-apm test
+apm-beta test -p /usr/bin/atom-beta
